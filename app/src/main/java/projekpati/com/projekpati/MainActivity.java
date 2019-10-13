a775e7c46fe2dc7eb5fadc373e8db001c0160274
setiawan <setiawanwenwen4@gmail.com>
2019-10-13 15:19:40 +0700

    meki

--- app/src/main/java/projekpati/com/projekpati/MainActivity.java
+++ app/src/main/java/projekpati/com/projekpati/MainActivity.java
@@ -6,21 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnKuliner;
+    private LinearLayout kulinerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //meki jalang bernama Andrew
 
 
-        btnKuliner = (Button) findViewById(R.id.btnKuliner);
-        btnKuliner.setOnClickListener(new View.OnClickListener() {
+        kulinerLayout =  findViewById(R.id.kulinerLinear);
+        kulinerLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, MenuKuliner.class);
