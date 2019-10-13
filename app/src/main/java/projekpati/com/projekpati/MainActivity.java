cca2fda4f5a14f1ff91db204c955ce76e9ebdad1
EENDRUU <christianandrew4321@gmail.com>
2019-10-13 20:27:13 +0700

    nav bar

--- app/src/main/java/projekpati/com/projekpati/MainActivity.java
+++ app/src/main/java/projekpati/com/projekpati/MainActivity.java
@@ -1,23 +1,80 @@
 package projekpati.com.projekpati;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private LinearLayout kulinerLayout;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dl= (DrawerLayout) findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
 
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+
+
+        //getActionBar().show();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                if(id==R.id.mEditProfil){
+                    Toast.makeText(MainActivity.this, "Edit Profile Clicked",Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.mPenanda){
+                    Toast.makeText(MainActivity.this, "Penanda Clicked",Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.mPemberitahuan){
+                    Toast.makeText(MainActivity.this, "Pemberitahuan Clicked",Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.mMenuIcon){
+                    Toast.makeText(MainActivity.this, "Menu Icon Clicked",Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.mReaksiAnda){
+                    Toast.makeText(MainActivity.this, "Reaksi Anda Clicked",Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.mInfoGempa){
+                    Toast.makeText(MainActivity.this, "Info Gempa Clicked",Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.mTentangKami){
+                    Toast.makeText(MainActivity.this, "Tentang Kami Clicked",Toast.LENGTH_SHORT).show();
+                }
+                else if(id==R.id.mMasukDaftar){
+                    Toast.makeText(MainActivity.this, "Masuk Daftar Clicked",Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
 
         kulinerLayout =  findViewById(R.id.kulinerLinear);
         kulinerLayout.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +85,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
