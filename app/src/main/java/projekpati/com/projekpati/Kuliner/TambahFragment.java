6f3776c5e08b6f6a8ccb20776c2171358c073c8b
EENDRUU <christianandrew4321@gmail.com>
2019-10-26 01:53:34 +0700

    post

--- app/src/main/java/projekpati/com/projekpati/Kuliner/TambahFragment.java
+++ app/src/main/java/projekpati/com/projekpati/Kuliner/TambahFragment.java
@@ -1,12 +1,22 @@
 package projekpati.com.projekpati.Kuliner;
 
 
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import projekpati.com.projekpati.API.API;
@@ -14,6 +24,7 @@ import projekpati.com.projekpati.API.RetrofitClientInstance;
 import projekpati.com.projekpati.Model.DetilKulinerBaru;
 import projekpati.com.projekpati.Model.KulinerModel;
 import projekpati.com.projekpati.Model.ListKuliner;
+import projekpati.com.projekpati.Model.MyItem;
 import projekpati.com.projekpati.R;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -25,45 +36,183 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.maps.android.clustering.Cluster;
+import com.google.maps.android.clustering.ClusterManager;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Map;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TambahFragment extends Fragment {
+public class TambahFragment extends Fragment implements OnMapReadyCallback, LocationListener {
 
 
     public TambahFragment() {
         // Required empty public constructor
     }
 
-    EditText Tnama,Talamat;
-    Button btnTambah;
+    GoogleMap mMap1, mMap2;
+    EditText eNama, ePemilik, eNomorTelp, eWebsite, eDeskripsi;
+    Button btnTambah, btnSetLocation;
+    SupportMapFragment mapFragment;
+    SupportMapFragment mapFragment1;
+    PlacesClient placesClient;
+
+    Marker currentLocation;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    LatLng startLatLng, currentLatLng;
+    CardView cardLocation;
+    Button simpanLocation;
+    RelativeLayout setMap;
+    ScrollView form;
+    LatLng location;
+    Marker markerLoc;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tambah, container, false);
 
-        Tnama = view.findViewById(R.id.eNama);
-       // Talamat = view.findViewById(R.id.mAlamat);
+        eNama = view.findViewById(R.id.eNama);
+        ePemilik = view.findViewById(R.id.mAlamat);
+        eNomorTelp = view.findViewById(R.id.mNomorTelp);
+        eWebsite = view.findViewById(R.id.mWebsite);
+        eDeskripsi = view.findViewById(R.id.mDeskripsi);
+        btnSetLocation = view.findViewById(R.id.btnLocation);
         btnTambah = view.findViewById(R.id.btnTambah);
+        setMap = view.findViewById(R.id.showMap);
+        form = view.findViewById(R.id.form);
 
         btnTambah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                addKuliner(Tnama.getText().toString());
+                addKuliner(eNama.getText().toString());
             }
         });
+
+        btnSetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                form.setVisibility(View.GONE);
+                setMap.setVisibility(View.VISIBLE);
+            }
+        });
+         cardLocation = (CardView) view.findViewById(R.id.btnSetLocations);
+        simpanLocation = (Button) view.findViewById(R.id.btnSimpanLocation);
+        simpanLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getContext(),"clicked",Toast.LENGTH_SHORT).show();
+                cardLocation.setVisibility(View.VISIBLE);
+                simpanLocation.setVisibility(View.GONE);
+                setMap.setVisibility(View.GONE);
+                form.setVisibility(View.VISIBLE);
+                location=currentLatLng;
+
+                mMap1.clear();
+                mMap1.addMarker(new MarkerOptions().position(location));
+                mMap1.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+
+            }
+        });
+        cardLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardLocation.setVisibility(View.GONE);
+                simpanLocation.setVisibility(View.VISIBLE);
+               // Toast.makeText(getContext(),"clicked",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), String.valueOf(currentLatLng.latitude),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+        startLatLng = new LatLng(-6.7487,111.0379);
+        currentLatLng = startLatLng;
+
+
+        mapFragment1 = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map2);
+        mapFragment1.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap1=googleMap;
+                mMap1.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                mMap1.clear();
+                mMap1.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-6.7487,111.0379),15));
+
+            }
+        });
+
+        mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap2=googleMap;
+                mMap2.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                mMap2.clear();
+                mMap2.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-6.7487,111.0379),15));
+
+                mMap2.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+                    @Override
+                    public void onCameraMove() {
+                        cardLocation.setVisibility(View.VISIBLE);
+                        simpanLocation.setVisibility(View.GONE);
+                    }
+                });
+                mMap2.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+                    @Override
+                    public void onCameraIdle() {
+
+                        LatLng center = mMap2.getCameraPosition().target;
+                        currentLatLng = center;
+                        Log.d("location", String.valueOf(center.latitude));
+                    }
+                });
+            }
+        });
+
+
+
+
+
+        setupAutoCompleteFragment();
+
+
+
+
 
 
         // Inflate the layout for this fragment
@@ -72,6 +221,56 @@ public class TambahFragment extends Fragment {
 
     }
 
+    private void setupAutoCompleteFragment() {
+        String apikey = getString(R.string.api_key);
+        if(!Places.isInitialized()){
+            Places.initialize(getContext(),apikey);
+        }
+
+        placesClient = Places.createClient(getContext());
+        final AutocompleteSupportFragment autocompleteSupportFragment=
+                (AutocompleteSupportFragment)  this.getChildFragmentManager().findFragmentById(R.id.autoComplete_fragment);
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.LAT_LNG));
+        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull final Place place) {
+                if(place.getLatLng()!= null) {
+                    mMap2.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+                        @Override
+                        public void onMapLoaded() {
+                            mMap2.moveCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15));
+                            //mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName()));
+                            currentLatLng= place.getLatLng();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
+    }
+
+
+
+    private BitmapDescriptor bitmapDescriptor(Context context, int vectorID)
+    {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context,vectorID);
+        vectorDrawable.setBounds(0,0,vectorDrawable.getIntrinsicWidth(),vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(),vectorDrawable.getIntrinsicHeight(),Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+
+    }
 
     public void addKuliner(String nama){
         //defining a progress dialog to show while signing up
@@ -99,4 +298,46 @@ public class TambahFragment extends Fragment {
 
     }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        if(currentLocation==null)
+        {
+            MarkerOptions options = new MarkerOptions();
+            options.position(startLatLng);
+            currentLocation = mMap2.addMarker(options);
+
+            mMap2.moveCamera(CameraUpdateFactory.newLatLngZoom(startLatLng,15));
+        }
+        else {
+            currentLocation.setPosition(startLatLng);
+        }
+        mMap2.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+            @Override
+            public void onCameraIdle() {
+                LatLng center = mMap2.getCameraPosition().target;
+
+                currentLocation.remove();
+                mMap2.addMarker(new MarkerOptions().position(center));
+                startLatLng = currentLocation.getPosition();
+                Log.d("current location", String.valueOf(startLatLng.latitude));
+
+
+            }
+        });
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 }
