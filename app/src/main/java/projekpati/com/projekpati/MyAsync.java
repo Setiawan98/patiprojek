c3a8d368b1b092d2fe30920e705089d462b25de7
setiawan <setiawanwenwen4@gmail.com>
2019-12-10 00:24:21 +0700

    pull dulu bos

--- app/src/main/java/projekpati/com/projekpati/MyAsync.java
+++ app/src/main/java/projekpati/com/projekpati/MyAsync.java
@@ -1,39 +1,4 @@
 package projekpati.com.projekpati;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
-/*
-public class MyAsync extends AsyncTask<Void, Void, Bitmap> {
-
-    */
-/*@Override
-    protected Bitmap doInBackground(Void... params) {
-
-        try {
-            URL url = new URL();
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-            return myBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }*//*
-
-}*/
